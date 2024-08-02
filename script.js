022290bcd4648f2f56f43147a6379659e45a3f9c
riyamishra28 <riya.mishra1303@gmail.com>
2024-08-02 20:58:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const searchCity = async() => {
+
+   // const city = document.getElementById('city-input').value;
+   const city = document.getElementById('city-input').value;
+    //console.log(city);
+    const data = await fetchWeatherData(city);
+    ShowData(data);
+}
+  
+
+const fetchWeatherData = async (city) => {
+    const url = `https://open-weather13.p.rapidapi.com/city/${city}/EN`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': 'b75bb28c0cmshbd5f237b3a35b85p1fbae8jsnc531f7b8e6ba',
+        'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
+      }
+    };
+    
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      const JsonResponse =  JSON.parse(result);
+
+      return JsonResponse;
+    } catch (error) {
+      console.error(error);
+    }
+};
+
+
+
+ShowData = (data) => {
+   // const weatherData = fetchWeatherData();
+    document.getElementById("city-name").innerText = data.name;
+    document.getElementById("weather-type").innerText = data.weather[0].main;
+    document.getElementById("temp").innerText = data.main.temp;
+    document.getElementById("min-temp").innerText = data.main.temp_min;
+    document.getElementById("max-temp").innerText = data.main.temp_max;
+  }
+
+
+
